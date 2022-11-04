5ec2685ebf864a5f9e5f5cac5797af3165c340ea
Jose Velasco <jose.juarez.velasco@gmail.com>
2022-11-04 15:30:13 -0700

    Created concrete animel: chicken

---
+++ src/Chicken.java
@@ -0,0 +1,5 @@
+public class Chicken extends Animal {
+    public Chicken(String species, int weight, boolean bidedal, int weightOfFood, int areaNeeded) {
+        super(species, weight, bidedal, weightOfFood, areaNeeded);
+    }
+}
